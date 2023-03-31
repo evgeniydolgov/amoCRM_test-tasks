59f0620db6570b1f1c7b0951010f95cf8a6a8fe2
Evgeniy Dolgov <amyr214519@gmail.com>
2023-03-31 12:28:55 +0300

    delete console log and comments

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
-
 let timerGo;
 const createTimerAnimator = () => {
   return (seconds) => {
@@ -14,7 +11,6 @@ const createTimerAnimator = () => {
       timerSeconds = seconds % 60;
       timerMinuts = Math.floor(seconds / 60) < 60 ? Math.floor(seconds / 60) : Math.floor((seconds % 3600) / 60);
       timerHours = Math.floor(seconds / 3600) > 60 ? Math.floor(seconds / 3600) : Math.floor((seconds % 216000) / 3600);
-      console.log(timerHours);
       timer.innerText = `${timerHours.toString().padStart(2, '0')}:${timerMinuts
         .toString()
         .padStart(2, '0')}:${timerSeconds.toString().padStart(2, '0')}`;
